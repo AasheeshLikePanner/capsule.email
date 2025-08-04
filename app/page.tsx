a91de6ec09ed296210c4484b4427bb8de2603470
AasheeshLikePanner <ashishlearningsoftware@gmail.com>
2025-08-05 01:06:21 +0530

    updating

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import dynamic from 'next/dynamic';
+import { Hero } from '@/components/hero';
 
-const DynamicHero = dynamic(() => import('@/components/hero'));
 
 export default function Home() {
   return (
     <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-      <DynamicHero />
+      <Hero />
     </div>
   );
 }
